e5d54782f0fb52778aff46f6885d325887f3e4b5
Alexander Strakhovenko <sasstr@gmail.com>
2020-01-21 23:35:10 +0300

    Начинаем канвасить

---
+++ js/stat.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var CLOUD = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  COORD_X: 100,
+  COORD_Y: 10,
+  GAP: 10,
+  BAR_HEIGTH: 20,
+  BAR_WIDTH: 20,
+  COLOR: 'rgb(0, 0, 0)',
+  SHADOW_COLOR: 'rgba(0, 0, 0, 0.7)',
+};
+
+var TEXT = {
+  FONT_GAP: 10,
+  FONT_SIZE: '16px',
+  FONT_FAMILY: 'PT Mono',
+  WIDTH: 50,
+  FIRST_LINE: 'Ура вы победили!',
+  SECOND_LINE: 'Список результатов:',
+};
+
+var getMaxResult = function () {
+
+};
+
+var renderCloud = function () {
+
+};
+
+window.renderStatistics = function (ctx, names, times) {
+
+
+};
