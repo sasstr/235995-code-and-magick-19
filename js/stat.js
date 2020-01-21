147b47219aa4f7669e75ed8c7ca0b0dfe7a778bc
Alexander Strakhovenko <sasstr@gmail.com>
2020-01-22 01:48:11 +0300

    Продолжаем конвасить

--- js/stat.js
+++ js/stat.js
@@ -6,8 +6,10 @@ var CLOUD = {
   COORD_X: 100,
   COORD_Y: 10,
   GAP: 10,
-  BAR_HEIGTH: 20,
-  BAR_WIDTH: 20,
+  BAR_GAP: 50,
+  BAR_COLOR: 'rgba(255, 0, 0, 1)',
+  BAR_HEIGTH: 150,
+  BAR_WIDTH: 40,
   COLOR: 'rgb(0, 0, 0)',
   SHADOW_COLOR: 'rgba(0, 0, 0, 0.7)',
 };
@@ -16,20 +18,39 @@ var TEXT = {
   FONT_GAP: 10,
   FONT_SIZE: '16px',
   FONT_FAMILY: 'PT Mono',
+  BASELINE: 'hanging',
   WIDTH: 50,
   FIRST_LINE: 'Ура вы победили!',
   SECOND_LINE: 'Список результатов:',
 };
 
-var getMaxResult = function () {
-
-};
-
-var renderCloud = function () {
-
-};
-
+/** Функция отрисовывает облако с результами игры ввиде гистограмм
+   * @param {object} ctx  контекст канваса
+   * @param {array} names массив, с именами игроков прошедших уровень
+   * @param {array} times Массив содержит время прохождения уровня соответствующего игрока из массива names.
+   *                      Время прохождения уровня задано в миллисекундах.
+   * @return {void} отрисовывает облако с результами игры ввиде гистограмм
+   */
 window.renderStatistics = function (ctx, names, times) {
 
+  /** Функция возращает возвращает координату мага на две трети по высоте экрана
+   * @return {number} возращает наибольший результат
+   */
+  var getMaxResult = function () {
+    return Math.max.apply(null, times);
+  };
+
+  // Функция возвращает цвет гистограммы
+  var getPlayerColor = function () {
+    return name === 'Вы' ? CLOUD.BAR_COLOR : 'hsl(235, 100%, ' + Math.random() * 100 + '%)';
+  };
+  // Функция рисует облако
+  var renderCloud = function () {
+
+  };
+
+  //
+
+  //
 
 };
